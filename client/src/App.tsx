890c979ee42470679171b5418ca7ea16184183b8
aaron ngu <aaronngu@hotmail.com>
2022-02-17 15:08:10 -0800

    Added Bypass-Tunnel-Reminder header to client (#5)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
-import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+import {ApolloClient, HttpLink, ApolloLink, InMemoryCache, concat, ApolloProvider} from '@apollo/client';
 import {RecoilRoot} from 'recoil';
 import HomeScreen from './containers/HomeScreen';
 
+const httpLink = new HttpLink({ uri: process.env.REACT_APP_BASE_URL });
+
+const handleTunnelingMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      'Bypass-Tunnel-Reminder': '2'
+    }
+  }));
+
+  return forward(operation);
+})
+
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_BASE_URL,
+  //uri: process.env.REACT_APP_BASE_URL,
+  link: concat(handleTunnelingMiddleware, httpLink),
   cache: new InMemoryCache(),
 });
 
